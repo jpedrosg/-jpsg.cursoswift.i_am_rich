e9276dd18f872d976ca3129c61acf874716e3abd
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-09-25 15:37:20 -0300

    finishing I Am Rich layout

--- I Am Rich/I Am Rich/ContentView.swift
+++ I Am Rich/I Am Rich/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich!")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(0.0)
+                    .frame(width: 200.0, height: 200.0)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
